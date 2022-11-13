192332745b306d0ce73270e8e45a193421f074de
Franklin Briones <franklinbriones@frankliionesmbp.lan>
2022-11-13 17:14:53 -0600

    begin creating dice components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Die from "./components/Die"
+
+
 
 function App() {
+
+const diceVals = Array.from({length: 10}, () => Math.floor(Math.random() * 10))
+const Dice = diceVals.map((diceVal) => {
+    <Die value={diceVal} />
+})
+
   return (
     <div className="App">
       <main className='main'>
+        <div className='dice-container'>
+          {Dice}
+        </div>
       </main>
     </div>
   );
