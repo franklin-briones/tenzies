208159ebefdb921af6d367c77e2c6049b8f8fbc9
Franklin Briones <franklinbriones@frankliionesmbp.lan>
2022-12-07 21:45:44 -0600

    conditional rendering in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './App.css';
 import Die from './components/Die';
-import {nanoid} from "nanoid"
+import { nanoid } from 'nanoid';
 
 /**
- * Challenge:
- *
- * Create state to hold our array of numbers. (Initialize
- * the state by calling our `allNewDice` function so it
- * loads all new dice as soon as the app loads)
- *
- * Map over the state numbers array to generate our array
- * of Die elements and render those in place of our
- * manually-written 10 Die elements.
+ * Challenge: Create a function `holdDice` that takes
+ * `id` as a parameter. For now, just have the function
+ * console.log(id).
+ * 
+ * Then, figure out how to pass that function down to each
+ * instance of the Die component so when each one is clicked,
+ * it logs its own unique ID property. (Hint: there's more
+ * than one way to make that work, so just choose whichever
+ * you want)
+ * 
  */
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice());
 
+  // Return array of length 10, each item is a dice object
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
@@ -25,16 +27,20 @@ function App() {
         id: nanoid(),
         value: Math.ceil(Math.random() * 6),
         isHeld: false
-      })
+      });
     }
     return newDice;
   }
 
+  // Helped function to create new array of dice
   function rollDice() {
     setDice(allNewDice());
   }
 
-  const diceElements = dice.map((die) => {return <Die key={die.id} value={die.value} />;});
+  // Map dice array to <Die /> elements
+  const diceElements = dice.map((die) => {
+    return <Die key={die.id} value={die.value} isHeld={die.isHeld} />;
+  });
 
   return (
     <div className="App">
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 function Die(props) {
+
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "#ffffff"
+    }
+
   return (
-    <div className="die">
-      <h3 className="die-num">{props.value}</h3>
+    <div className="die" style={styles}>
+        <h3 className="die-num">{props.value}</h3>
     </div>
+    
   );
 }
 
